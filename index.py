d36833d0e0574260c53d3f901fa091063b40becb
petervaro <petervaro@sketchandprototype.com>
2015-06-30 10:39:49 +0200

    fork-us badge added; spaces are now nbsp; returns are br

--- index.py
+++ index.py
@@ -31,13 +31,14 @@ class Span(Tag):
     def __init__(self, variable):
         self.v = variable
 
-TAGS = (Span('comment'),
-        Span('entity'),
-        Span('class'),
-        Span('member'),
-        Span('operator'),
-        Span('string'),
-        Span('const'))
+TAGS = (Span('comment'),    # 0
+        Span('entity'),     # 1
+        Span('class'),      # 2
+        Span('member'),     # 3
+        Span('operator'),   # 4
+        Span('string'),     # 5
+        Span('const'),      # 6
+        Span('decorator'))  # 7
 
 HEADER = ('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">'
           '<title>Come in, We are OPEN!</title>'
@@ -47,35 +48,44 @@ HEADER = ('<!DOCTYPE html><html lang="en"><head><meta charset="utf-8">'
           '</head><body><div id="main"><pre><code>')
 FOOTER = '</code></pre></div></body></html>'
 
-PAGE = """
-{0.o}─────────────────────────────────────────────────────────────┬──────────────────{0.c}
-                                                             {0.o}│{0.c} {1.o}┏━━━━━┓{1.c}
- Come In, We Are Open!                                       {0.o}│{0.c} {1.o}┃     ┃{1.c} Kitchen
- Next Hackathon: 24-25th July                                {0.o}│{0.c} {1.o}┃     ┃{1.c} Budapest
-                                                             {0.o}│{0.c} {1.o}┗━━━━━┛{1.c}
-{0.o}─────────────────────────────────────────────────────────────┴──────────────────{0.c}
-
-{2.o}news{2.c}
-{{
-    {3.o}.next_hackathon{3.c} {4.o}={4.c} {{{6.o}7{6.c}, {6.o}8{6.c}, {5.o}"August"{5.c}}};
+PAGE = """\
+                                                           {7.o}╲{7.c}   {7.o}╲{7.c}
+                                                            {7.o}╲{7.c} F {7.o}╲{7.c}
+                                                             {7.o}╲{7.c} O {7.o}╲{7.c}
+                                                              {7.o}╲{7.c} R {7.o}╲{7.c}
+                                                               {7.o}╲{7.c} K {7.o}╲{7.c}
+{0.o}─────────────────────────────────────────────┬──────────────────{0.c}{7.o}╲{7.c}   {7.o}╲{7.c}{0.o}───────────{0.c}
+                                             {0.o}│{0.c} {1.o}┏━━━━━┓{1.c}           {7.o}╲{7.c} U {7.o}╲{7.c}
+ Come In, We Are Open!                       {0.o}│{0.c} {1.o}┃     ┃{1.c} Kitchen    {7.o}╲{7.c} S {7.o}╲{7.c}
+ Next Hackathon: 24-25th July                {0.o}│{0.c} {1.o}┃     ┃{1.c} Budapest    {7.o}╲{7.c}   {7.o}╲{7.c}
+                                             {0.o}│{0.c} {1.o}┗━━━━━┛{1.c}              {7.o}╲{7.c} O {7.o}╲{7.c}
+{0.o}─────────────────────────────────────────────┴───────────────────────{0.c}{7.o}╲{7.c} N {7.o}╲{7.c}{0.o}──────{0.c}
+                                                                      {7.o}╲{7.c}   {7.o}╲{7.c}
+{2.o}news{2.c}                                                                   {7.o}╲{7.c} G {7.o}╲{7.c}
+{{                                                                       {7.o}╲{7.c} I {7.o}╲{7.c}
+    {3.o}.next_hackathon{3.c} {4.o}={4.c} {{{6.o}7{6.c}, {6.o}8{6.c}, {5.o}"August"{5.c}}};                                  {7.o}╲{7.c} T {7.o}╲{7.c}
+}}                                                                         {7.o}╲{7.c} H {7.o}╲{7.c}
+                                                                           {7.o}╲{7.c} U {7.o}╲{7.c}
+{2.o}links{2.c}                                                                       {7.o}╲{7.c} B
+{{                                                                            {7.o}╲{7.c}
+    {3.o}.github{3.c}    {4.o}={4.c} {5.o}"https://github.com/kitchenbudapest"{5.c};                        {7.o}╲{7.c}
+    {3.o}.more_info{3.c} {4.o}={4.c} {5.o}"http://www.kibu.hu"{5.c};                                         {7.o}╲{7.c}
 }}
 
-{2.o}links{2.c}
-{{
-    {3.o}.github{3.c}    {4.o}={4.c} {5.o}"https://github.com/kitchenbudapest"{5.c};
-    {3.o}.more_info{3.c} {4.o}={4.c} {5.o}"http://www.kibu.hu"{5.c};
-}}
-
-{0.o}────────────────────────────────────────────────────────────────────────────────{0.c}
-""".strip()
+{0.o}────────────────────────────────────────────────────────────────────────────────{0.c}\
+"""
 
 print(PAGE.format(*(Null(),)*len(TAGS)))
 
 def to_html(string):
     buffer = []
     for char in string:
+        if char == ' ':
+            buffer.append('&nbsp;')
+        elif char == '\n':
+            buffer.append('</br>')
         # If ASCII character
-        if char in printable:
+        elif char in printable:
             buffer.append(char)
         # If special character
         else:
