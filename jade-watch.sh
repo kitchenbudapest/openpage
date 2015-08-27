323d3fbdeec10db01708e90a20e5af1e6faf30d5
petervaro <petervaro@sketchandprototype.com>
2015-08-27 17:00:06 +0200

    closes #12; full rewrite of html => jade; jade-watcher shell script added

---
+++ jade-watch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+## INFO ##
+## INFO ##
+
+INDEX=index.jade
+
+keyboard_interrupt()
+{
+    printf '\nExiting.\n'
+}
+
+trap keyboard_interrupt SIGINT
+printf 'Start watching $INDEX for changes:'
+jade -O '{"MINIFIED": true}' --watch $INDEX &
+jade -P -E test.html --watch $INDEX &
+wait
